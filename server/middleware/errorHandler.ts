fd50771c076eba5cb7f80cb887df43dd86d2c1cc
ConlynPattison <cpattison@csumb.edu>
2024-06-03 01:38:22 -0700

    Added MongoDB and experimentation schema

--- server/middleware/errorHandler.ts
+++ server/middleware/errorHandler.ts
@@ -1,6 +1,13 @@
 import { NextFunction, Request, Response } from "express";
 import { logEvents } from "./logger";
 
+/**
+ * Middleware function for logging each error's name, message, and request method, url, and origin in logs.
+ * @param err 
+ * @param req 
+ * @param res 
+ * @param next 
+ */
 export const errorHandler = (err: Error, req: Request, res: Response, next: NextFunction) => {
 	logEvents(`${err.name}: ${err.message}\t${req.method}\t${req.url}\t${req.headers.origin}`, "errLog.log");
 	console.log(err.stack);
