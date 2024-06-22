5b47ab5214f9752ae59351c2dadf725af6f228e0
ConlynPattison <cpattison@csumb.edu>
2024-06-22 00:52:39 -0700

    Added zod parsing to team controller, simplified return statements

--- server/controllers/teamController.ts
+++ server/controllers/teamController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express"
 import Team from "../models/Team";
+import { z } from "zod";
+import mongoose from "mongoose";
 
 /** 
  * @desc Get all teams
@@ -13,6 +15,14 @@ export const getAllTeams = async (req: Request, res: Response) => {
 	res.json(teams);
 }
 
+const createNewTeamSchema = z.object({
+	body: z.object({
+		name: z.string(),
+		ownerId: z.string(),
+		adminIds: z.string().array().optional()
+	})
+});
+
 /**
  * @desc Create a new team
  * @route POST /teams
@@ -21,26 +31,25 @@ export const getAllTeams = async (req: Request, res: Response) => {
  * @param {Response} res - Express response object
  */
 export const createNewTeam = async (req: Request, res: Response) => {
-	// todo: change this to a zod parse
-	const { name, ownerId, adminIds } = req.body;
+	const { success, data, error } = createNewTeamSchema.safeParse(req);
 
-	if (adminIds && !Array.isArray(adminIds)) {
-		res.status(400).json({ message: "Admin id(s) must be passed in an array" });
-		return;
+	if (!success) {
+		return res.status(400).json({ message: error.issues });
 	}
 
-	if (!name || !ownerId) {
-		res.status(400).json({ message: "All fields required" });
-		return;
-	}
+	const { name, ownerId, adminIds } = data.body;
+
+	// This should now be redundant, but leaving for now.
+	// if (adminIds && !Array.isArray(adminIds)) {
+	// 	res.status(400).json({ message: "Admin id(s) must be passed in an array" });
+	// 	return;
+	// }
 
 	const duplicate = await Team.findOne({ name, owner_id: ownerId }).lean().exec();
 	if (duplicate) {
-		res.status(409).json({ message: `Team with name ${name} already exists under user id ${ownerId}` });
-		return;
+		return res.status(409).json({ message: `Team with name ${name} already exists under user id ${ownerId}` });
 	}
 
-	// todo: revist after zod integration
 	const hasValidAdminIds = adminIds && Array.isArray(adminIds);
 
 	const teamObject = {
@@ -57,6 +66,15 @@ export const createNewTeam = async (req: Request, res: Response) => {
 	}
 }
 
+const updateTeamSchema = z.object({
+	body: z.object({
+		id: z.string(),
+		name: z.string(),
+		ownerId: z.string(),
+		adminIds: z.string().array()
+	})
+});
+
 /**
  * @desc Update a team
  * @route PATCH /teams
@@ -65,40 +83,47 @@ export const createNewTeam = async (req: Request, res: Response) => {
  * @param {Response} res - Express response object
  */
 export const updateTeam = async (req: Request, res: Response) => {
-	// todo: change this to a zod parse
-	const { id, name, ownerId, adminIds } = req.body;
+	const { success, error, data } = updateTeamSchema.safeParse(req);
+
+	if (!success) {
+		return res.status(400).json({ message: error.issues });
+	}
+
+	const { id, name, ownerId, adminIds } = data.body;
 
 	if (adminIds && !Array.isArray(adminIds)) {
-		res.status(400).json({ message: "Admin id(s) must be passed in an array" });
-		return;
+		return res.status(400).json({ message: "Admin id(s) must be passed in an array" });
 	}
 
 	if (!id || !name || !ownerId) {
-		res.status(400).json({ message: "All fields required" });
-		return;
+		return res.status(400).json({ message: "All fields required" });
 	}
 
 	const team = await Team.findById(id).exec();
 
 	if (!team) {
-		res.status(400).json({ message: "Team not found" });
-		return;
+		return res.status(400).json({ message: "Team not found" });
 	}
 
 	const duplicate = await Team.findOne({ name, owner_id: ownerId }).lean().exec();
-	if (duplicate && duplicate._id !== id) {
-		res.status(409).json({ message: `Another team with name ${name} already exists under user id ${ownerId}` });
-		return;
+	if (duplicate && duplicate._id.toString() !== id) {
+		return res.status(409).json({ message: `Another team with name ${name} already exists under user id ${ownerId}` });
 	}
 
 	team.name = name;
 	// todo: do we want owner_id to be something that can even be changed?
-	team.owner_id = ownerId;
-	team.admin_ids = adminIds;
+	team.owner_id = new mongoose.Types.ObjectId(ownerId);
+	team.admin_ids = adminIds.map(id => new mongoose.Types.ObjectId(id));
 
 	const updatedTeam = await team.save();
 	res.json({ message: `${updatedTeam.name} updated` });
 }
+
+const deleteTeamSchema = z.object({
+	body: z.object({
+		id: z.string()
+	})
+});
 
 /**
  * @desc Remove a team
@@ -108,20 +133,19 @@ export const updateTeam = async (req: Request, res: Response) => {
  * @param {Response} res - Express response object
  */
 export const deleteTeam = async (req: Request, res: Response) => {
-	// todo: change this to a zod parse
-	const { id } = req.body;
+	const { success, error, data } = deleteTeamSchema.safeParse(req);
 
-	if (!id) {
-		res.status(400).json({ message: "Team id field required" });
-		return;
+	if (!success) {
+		return res.status(400).json({ message: "Team id field required" });
 	}
+
+	const { id } = data.body;
 
 	// todo: Add any checks that should be done before deleting a team (surveys, results, etc)
 	const team = await Team.findById(id).exec();
 
 	if (!team) {
-		res.status(400).json({ message: "Team not found" });
-		return;
+		return res.status(400).json({ message: "Team not found" });
 	}
 
 	const result = await team.deleteOne();
